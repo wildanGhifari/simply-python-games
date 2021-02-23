4c68f409a3d8644d7fa5ad45983ed38e6f96eb0c
wildanGhifari <wildghifari@gmail.com>
2021-02-24 00:22:44 +0700

    Make several if...else statement and finish the game, bye

--- main.py
+++ main.py
@@ -3,11 +3,43 @@ print("Welcome to my first game!")
 name = input("What is your name? ")
 age = int(input("What is your age? "))
 
-print("Hello", name, "you are", age, "years old.")
+health = 10
 
 if age >= 18:
     print("You are old enough!")
-elif age >= 13:
-    print("You can play with supervision")
+
+    wants_to_play = input("Do you want to play? (yes/no)\n").lower()
+    if wants_to_play == "yes":
+        print("You are starting with", health, "health")
+        print("Let's play!")
+
+        left_or_right = input("First choice... Left or Right (left/right)?\n")
+        if left_or_right == "right":
+            ans = input(
+                "Nice, you follow the path and reach a lake... Do you swim across or go around (across/around)?\n")
+            if ans == "around":
+                print("You went around and reach the other side of the lake")
+            elif ans == "across":
+                print(
+                    "You manage to get across, but were bit by a fish and lost 5 health.")
+                health -= 5
+            ans = input(
+                "you notice a house and a river. Which do you go to (river/house)?\n")
+            if ans == "house":
+                print(
+                    "You go to the house and greeted by the owner... He doesn't like you and you lose 5 health")
+                health -= 5
+                if health <= 0:
+                    print("You now have 0 healht and you lose the game...")
+                else:
+                    print("You have survived... You win!")
+            else:
+                print("You fell in the river and lost...")
+        else:
+            print("You fell down and lost...")
+
+    else:
+        print("Cya...")
+
 else:
     print("You are not old enough to play..")
